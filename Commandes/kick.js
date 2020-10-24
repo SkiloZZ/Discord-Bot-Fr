75476e8a297b5331cf421b0b7e918bf2e33588bb
SkiloZ <50947369+SkiloZZ@users.noreply.github.com>
2020-10-24 02:07:55 +0200

    Update kick.js

--- Commandes/kick.js
+++ Commandes/kick.js
@@ -4,16 +4,16 @@ module.exports.run = (client, message) => {
     const member = message.mentions.members.first()
     
     if (!member) {
-    return message.reply(`Il faut que tu mentionnes une personne.`)
+    return message.reply(`il faut que tu mentionnes une personne.`)
     }
-    if(!message.member.hasPermission("KICK_MEMBERS")) return message.reply("Vous n'avez pas la permission d'expulser.")
+    if(!message.member.hasPermission("KICK_MEMBERS")) return message.reply("tu n'as pas la permission d'expulser.")
     if (!member.kickable) {
-    return message.reply(`Je ne peux pas kick ce membre.`)
+    return message.reply(`je ne peux pas expulser ce membre.`)
     }
     return member
     .kick()
     .then(() => message.reply(`${member.user.tag} a été kick.`))
-    .catch(error => message.reply("Une erreur s'est produite."))
+    .catch(error => message.reply("une erreur s'est produite."))
 }
   
   
